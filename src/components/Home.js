dbbc32c84c87db6a6456226deb70da3d5548c985
benoit barges <benoitbarges@gmail.com>
2020-12-10 10:30:37 +0100

    teams

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { getTeamNames } from '../api'
+
+import TeamLogo from './TeamLogo'
 
 export default class Home extends Component {
+  state = {
+    teamNames: []
+  }
+
+  componentDidMount() {
+    getTeamNames()
+      .then(teamNames => this.setState({ teamNames }))
+  }
+
   render() {
     return (
       <div className='container'>
-        Home
+        <h1 className='large-header'>
+          Hash History Basketball League
+        </h1>
+
+        <h3 className='header text-center'>
+          Select a team
+        </h3>
+        <div className='home-grid'>
+          {this.state.teamNames.map((name) => (
+            <Link key={name} to={`/${name}`}>
+              <TeamLogo id={name}/>
+            </Link>
+          ))}
+        </div>
       </div>
     )
   }
