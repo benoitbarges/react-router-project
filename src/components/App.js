ae305eed56c15f5e8926f0d4b704490c45f1628a
benoit barges <benoitbarges@gmail.com>
2020-12-12 11:13:31 +0100

    lazy import

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Home from './Home'
-import Players from './Players'
-import Teams from './Teams'
-import TeamPage from './TeamPage'
 import Navbar from './Navbar'
-import Articles from './Articles'
+import Loading from './Loading'
+
+const Home = React.lazy(() => import('./Home'))
+const Teams = React.lazy(() => import('./Teams'))
+const TeamPage = React.lazy(() => import('./TeamPage'))
+const Articles = React.lazy(() => import('./Articles'))
+const Players = React.lazy(() => import('./Players'))
 
 function FourOfour() {
   return <h1 className='text-center'>404 Not Found</h1>
@@ -19,14 +21,16 @@ class App extends Component {
         <div>
           <Navbar />
 
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/players' component={Players} />
-            <Route path='/teams' component={Teams} />
-            <Route path='/:teamId/articles' component={Articles} />
-            <Route exact path='/:teamId' component={TeamPage} />
-            <Route component={FourOfour} />
-          </Switch>
+          <React.Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path='/' render={() => <Home />} />
+              <Route path='/players' render={() => <Players />} />
+              <Route path='/teams' render={() => <Teams />} />
+              <Route path='/:teamId/articles' render={() => <Articles />} />
+              <Route exact path='/:teamId' render={() => <TeamPage />} />
+              <Route component={FourOfour} />
+            </Switch>
+          </React.Suspense>
         </div>
       </Router>
     )
